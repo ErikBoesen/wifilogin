57c21cc52792e9a8e67b1dafb788434ad6c28c8e
ErikBoesen <me@erikboesen.com>
2018-11-30 18:36:19 -0500

    login.py: add all parameters probably necessary for logging in

---
+++ login.py
@@ -0,0 +1,15 @@
+import requests
+# TODO: Do we need to include mac address parameters etc.?
+TARGET_URL = 'https://guestwifi.fccps.org/login.html'
+
+payload = {
+    # TODO: Figure out what 4 means; it's statically set upon clicking agree
+    'buttonClicked': '4',
+    # TODO: Does it matter what goes here? I don't think so.
+    'redirect_url': 'https://erikboesen.com',
+    'err_flag': '0',
+    'persistent': '1'  # for cookies or something?
+}
+session = requests.session()
+requests.post(TARGET_URL, data=payload)
+print(r.response)
