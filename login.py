3e2b3e9f1b423f04f657e93d7e1e73e0efc5e2bd
ErikBoesen <me@erikboesen.com>
2018-11-30 18:51:12 -0500

    Return JSON from request properly

--- login.py
+++ login.py
@@ -1,9 +1,10 @@
 import requests
 # TODO: auto-detect target URL? Could try sending a request to a known page and seeing what we get back?
 # TODO: Do we need to include mac address parameters etc.?
+# TODO: Must we give a user agent?
 TARGET_URL = 'https://guestwifi.fccps.org/login.html'
 
-payload = {
+form = {
     # TODO: Figure out what 4 means; it's statically set upon clicking agree
     'buttonClicked': '4',
     # TODO: Does it matter what goes here? I don't think so.
@@ -12,5 +13,5 @@ payload = {
     'persistent': '1'  # for cookies or something?
 }
 session = requests.session()
-requests.post(TARGET_URL, data=payload)
-print(r.response)
+r = requests.post(TARGET_URL, data=form)
+print(r.json())
