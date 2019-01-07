0d465ec30af1ad5080ae0b69c6e8e158e10ae96b
ErikBoesen <me@erikboesen.com>
2019-01-07 17:08:31 -0500

    Add long URL

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ import requests
 # TODO: Do we need to include mac address parameters etc.?
 # TODO: Must we give a user agent?
 TARGET_URL = 'https://guestwifi.fccps.org/login.html'
+TARGET_URL = 'https://guestwifi.fccps.org/fs/customwebauth/login.html?switch_url=https://guestwifi.fccps.org/login.html&ap_mac=50:17:ff:b3:e5:90&client_mac=d4:61:9d:25:2a:f6&wlan=FCCPS-Guest&redirect=detectportal.firefox.com/success.txt'
 
 form = {
     # TODO: Figure out what 4 means; it's statically set upon clicking agree
